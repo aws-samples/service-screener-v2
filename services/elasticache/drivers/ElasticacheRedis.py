050c7e57cf3a3b380008288eb7ba4692dd121990
CY Khoo <cykhoo.norm@gmail.com>
2023-10-19 11:35:52 +0000

    Add in error handling for while cluster under creating status

--- services/elasticache/drivers/ElasticacheRedis.py
+++ services/elasticache/drivers/ElasticacheRedis.py
@@ -8,6 +8,11 @@ class ElasticacheRedis(ElasticacheCommon):
         # self.init()
 
     def _checkDefaultPort(self):
+        
+        ## Error handling when cluster is creating
+        if self.cluster.get('CacheClusterStatus') in ['creating', 'deleting', 'deleted']:
+            return
+        
         ports_in_cluster = [node.get('Endpoint').get(
             'Port') for node in self.cluster.get('CacheNodes')]
         if any(port == ElasticacheRedis.defaultPort for port in ports_in_cluster):
