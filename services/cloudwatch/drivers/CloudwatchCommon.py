370c4b0fb33f96f5e9ad3e2c76cf7e4e321016e9
yingting <yingting@amazon.com>
2024-12-23 16:04:14 +0800

    added one cw check

--- services/cloudwatch/drivers/CloudwatchCommon.py
+++ services/cloudwatch/drivers/CloudwatchCommon.py
@@ -20,9 +20,13 @@ class CloudwatchCommon(Evaluator):
     def __init__(self, log, logClient):
         super().__init__()
         self.init()
-        
+        ssBoto = self.ssBoto
+
         self.log = log
         self.logClient = logClient
+        self.cwClient = ssBoto.client('cloudwatch', config=self.bConfig)
+        self.snsClient = ssBoto.client('sns', config=self.bConfig)
+
         return
     
     ###### TO DO #####
@@ -33,3 +37,25 @@ class CloudwatchCommon(Evaluator):
             self.results['SetRetentionDays'] = [-1, "{} MB".format(self.log['storedBytes']/1024/1024)]
         elif self.log['retentionInDays'] <= 365:
             self.results['CISRetentionAtLeast1Yr'] = [-1, self.log['retentionInDays']]
+
+    
+    ###### TO DO #####
+    ## Done by YT. Check if this function make sense/ work
+    def _check_alarm_subscriptions(self):
+        paginator = self.cwClient.get_paginator('describe_alarms')
+        has_subscription = False
+        for page in paginator.paginate():
+            for alarm in page['MetricAlarms']:
+                # Check if alarm has SNS actions
+                if alarm.get('AlarmActions'):
+                    for action in alarm['AlarmActions']:
+                        if action.startswith('arn:aws:sns'):
+                            # Get SNS topic subscriptions
+                            response = self.snsClient.list_subscriptions_by_topic(
+                                TopicArn=action
+                            )
+                            if response['Subscriptions']:
+                                has_subscription = True
+                                break
+        if not has_subscription:
+            self.result['hasNotifications'] = [-1, '']
